78e75b4d2d259182b6851ae52c745598b240c29a
Kokichi Omi <celsus558@ybb.ne.jp>
2018-06-22 04:18:43 +0000

    7th commit

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,8 @@
 class Post < ApplicationRecord
   validates :content, length: { in: 1..140 }
+  validates :name,  presence: true, length: { maximum: 30 }
+  validates :email, presence: true, length: { maximum: 255 },
+                    format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
   mount_uploader :image, ImageUploader
   belongs_to :user
   default_scope -> { order(created_at: :desc) }
